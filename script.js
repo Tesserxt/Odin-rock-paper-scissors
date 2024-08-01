33c2a323ad07fbd1881d1428b834bf322446a137
Tesserxt <142136334+Tesserxt@users.noreply.github.com>
2024-08-01 06:41:39 +0530

    Added getComputerChoice() to script.js
    -returns between rock, paper, or scissors.

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log(Math.random())
+
+function getComputerChoice()
+{
+    choice = undefined;
+    rnd_value = Math.random();
+    
+    if(rnd_value < 0.33){
+        choice = "rock";
+    }
+    else if(rnd_value > 0.66){
+        choice = "paper";
+    }
+    else{
+        choice = "scissors";
+    }
+    return choice;
+}
+
+console.log(getComputerChoice());

53f3b45b64b25d36bf172bc476b76772bf291f83
Tesserxt <142136334+Tesserxt@users.noreply.github.com>
2024-08-01 08:31:39 +0530

    Added functions & vars to script.js
    - computerScore, humanScore to global scope
    - playRound(humanChoice, computerChoice)
    - playGame() /calls playround until player wons

--- script.js
+++ script.js
@@ -19,7 +19,48 @@ function getComputerChoice()
 
 function getHumanChoice()
 {
-   let choice = prompt("Choose between rock, Ppper, or scissors.");
+   let choice = prompt("Choose between rock, paper, or scissors.");
    return choice.toLowerCase();
 }
-console.log(getHumanChoice());
+
+function playRound(humanChoice, computerChoice)
+{
+    console.log( "you chose " + humanChoice);
+    let result;
+    if(humanChoice === computerChoice){
+        result = "Tie!";
+    }
+    else if(humanChoice === "rock" && computerChoice === "scissors"){
+        humanScore++; 
+        result = `You win! ${humanChoice} beats ${computerChoice}`;
+    }
+    else if(humanChoice === "scissors" && computerChoice === "paper"){
+        humanScore++;
+        result = `You win! ${humanChoice} beats ${computerChoice}`;
+    }
+    else if(humanChoice === "paper" && computerChoice === "rock"){
+        humanScore++;
+        result = `You win! ${humanChoice} beats ${computerChoice}`;
+    }
+    else{
+        computerScore++;
+        result = `You Lose! ${computerChoice} beats ${humanChoice}`;
+    }
+    console.log(result);
+}
+
+function playGame()
+{
+    while(humanScore < 3 && computerScore < 3){
+        let humanChoice = getHumanChoice();
+        let computerChoice = getComputerChoice();
+        playRound(humanChoice, computerChoice);
+    }
+    console.log("Game Over!")
+    console.log("SCORES! {You} {Computer} : ", humanScore, computerScore);
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+playGame();
